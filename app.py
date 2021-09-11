a5e87f5e1d2627f6be2b9af9b4690b52da155185
Alex <aphilay@gmail.com>
2021-09-11 01:53:25 -0700

    added comments

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ import paho.mqtt.client as mqtt
 import json
 
 id = '0eae5b2e-4fb0-42cb-a639-f824ccaae930'
-
 client_name = id + 'nightlight_client'
 client_telemetry_topic = id + '/telemetry'
 server_command_topic = id + '/commands'
 
 mqtt_client = mqtt.Client(client_name)
 mqtt_client.connect('test.mosquitto.org')
+
+light_sensor = GroveLightSensor(0)
+led = GroveLed(5)
 
 
 def handle_command(client, userdata, message):
@@ -26,6 +28,7 @@ def handle_command(client, userdata, message):
     else:
         led.off()
 
+#subscribed to the server_command topic
 mqtt_client.subscribe(server_command_topic)
 mqtt_client.on_message = handle_command
 
@@ -33,17 +36,13 @@ mqtt_client.loop_start()
 
 print("MQTT connected!")
 
-light_sensor = GroveLightSensor(0)
-led = GroveLed(5)
-
-
-
 while True:
     light = light_sensor.light
     telemetry = json.dumps({'light' : light})
 
     print("Sending telemetry ", telemetry)
 
+    #publish to server using the telemetry topic
     mqtt_client.publish(client_telemetry_topic, telemetry)
 
     time.sleep(5)
